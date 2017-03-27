c17ee8dab2026bb40787028948d7fa8100d7afce
abosii <alexandru.bosii@endava.com>
2017-03-27 14:08:48 +0300

    documentation

--- spark/src/main/java/com/eter/spark/data/database/impl/spark/SparkSQLProperties.java
+++ spark/src/main/java/com/eter/spark/data/database/impl/spark/SparkSQLProperties.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.Properties;
 
 /**
- * Created by rusifer on 3/26/17.
+ * Describe spark sql session properties.
  */
 public class SparkSQLProperties implements DatabaseProperties {
     private Map<String, String> properties;
@@ -17,27 +17,54 @@ public class SparkSQLProperties implements DatabaseProperties {
         properties = new HashMap<>();
     }
 
+    /**
+     * Put database property as key-value.
+     *
+     * @param key   property key
+     * @param value property value
+     */
     @Override
     public void put(String key, String value) {
         properties.put(key, value);
     }
 
+    /**
+     * Get database property value.
+     *
+     * @param key property key
+     * @return property value
+     */
     @Override
     public String get(String key) {
         return properties.get(key);
     }
 
+    /**
+     * Return whole map with all stored properties.
+     *
+     * @return map with stored properties
+     */
     @Override
     public Map<String, String> getAsMap() {
         return properties;
     }
 
+    /**
+     * Using config file from where to read properties.
+     *
+     * @param path path to file.
+     */
     @Override
     public void applyConfigFile(String path) {
         this.configFile = path;
 
     }
 
+    /**
+     * Return native spark sql properties.
+     *
+     * @return native spark sql properties
+     */
     public Properties getAsSupportedProprietes() {
         Properties sqlProperties = new Properties();
         sqlProperties.putAll(properties);
