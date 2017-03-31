e83f6376b3920c06dc4d6ab75f2d791c10ce0ec9
abosii <alexandru.bosii@endava.com>
2017-03-31 16:13:53 +0300

    acquire multiple cases and entity types

--- spark/src/test/java/com/eter/spark/data/dao/impl/spark/SparkSQLDAOTest.java
+++ spark/src/test/java/com/eter/spark/data/dao/impl/spark/SparkSQLDAOTest.java
@@ -3,9 +3,7 @@ package com.eter.spark.data.dao.impl.spark;
 import com.eter.spark.data.database.DatabaseProperties;
 import com.eter.spark.data.database.impl.spark.SparkSQLConnection;
 import com.eter.spark.data.database.impl.spark.SparkSQLProperties;
-import com.eter.spark.data.entity.Category;
-import com.eter.spark.data.entity.Customer;
-import com.eter.spark.data.entity.Product;
+import com.eter.spark.data.entity.*;
 import com.eter.spark.data.util.transform.reflect.EntityReflection;
 import com.eter.spark.data.util.transform.reflect.MethodSolver;
 import com.eter.spark.data.util.dao.SparkSQLRelationResolver;
@@ -13,21 +11,24 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
- * Created by rusifer on 3/26/17.
+ * Testing DAO object based on Spark SQL implementation
  */
 public class SparkSQLDAOTest {
-    SparkSQLConnection connection;
-    SparkSQLDAO dao;
+    static SparkSQLConnection connection;
+    static SparkSQLDAO dao;
 
+    /**
+     * Setting up PostgreSQL database
+     *
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
 //        connection = new SparkSQLConnection();
@@ -44,8 +45,13 @@ public class SparkSQLDAOTest {
 //        dao.setDatabaseConnection(connection);
     }
 
-    @Before
-    public void setupSQLServer() throws Exception {
+    /**
+     * Setting up Microsoft SQL Server
+     *
+     * @throws Exception
+     */
+    @BeforeClass
+    public static void setUpSQLServer() throws Exception {
 
         connection = new SparkSQLConnection();
         DatabaseProperties properties = new SparkSQLProperties();
@@ -62,18 +68,95 @@ public class SparkSQLDAOTest {
 
     }
 
-    @After
-    public void tearDown() throws Exception {
+    /**
+     * Close current sql connection
+     *
+     * @throws Exception
+     */
+    @AfterClass
+    public static void tearDown() throws Exception {
         connection.close();
     }
 
+
     @Test
+    public void testGetAllProducts() {
+        assert (dao.getAll(Product.class).size() != 0);
+    }
+
+    @Test
+    public void testGetAllOrders() {
+        assert (dao.getAll(Order.class).size() != 0);
+    }
+
+    @Test
+    public void testGetAllOrdersDetail() {
+        assert (dao.getAll(OrderDetail.class).size() != 0);
+    }
+
+    @Test
+    public void testGetAllCustomers() {
+        assert (dao.getAll(Customer.class).size() != 0);
+    }
+
+    @Test
+    public void testGetAllCustomersDetail() {
+        assert (dao.getAll(CustomerDetail.class).size() != 0);
+    }
+
+    @Test
+    public void testGetAllCategories() {
+        assert (dao.getAll(Category.class).size() != 0);
+    }
+
+    @Test
+    public void testRelationCustomerCustomerDetail() {
+        Dataset<Row> customers = dao.getAllAsDataset(Customer.class);
+
+        SparkSQLRelationResolver.resolveOneToOne(dao, customers, Customer.class)
+                .foreach((objectCustomer) -> {
+                    assert (objectCustomer.getCustomerDetail() != null);
+                });
+    }
+
+    @Test
+    public void testRelationProductCategory() {
+        Dataset<Row> products = dao.getAllAsDataset(Product.class);
+
+        SparkSQLRelationResolver.resolveOneToOne(dao, products, Product.class)
+                .foreach((objectProduct) -> {
+                    assert (objectProduct.getCategory() != null);
+                });
+    }
+
+    @Test
+    public void testRelationOrderOrderDetailCustomer() {
+        Dataset<Row> orders = dao.getAllAsDataset(Order.class);
+
+        SparkSQLRelationResolver.resolveOneToOne(dao, orders, Order.class)
+                .foreach((objectOrder) -> {
+                    assert (objectOrder.getCustomer() != null);
+                    assert (objectOrder.getOrderDetail() != null);
+                });
+    }
+
+    @Test
+    public void testRelationOrderDetailCustomerProduct() {
+        Dataset<Row> ordersDetails = dao.getAllAsDataset(OrderDetail.class);
+
+        SparkSQLRelationResolver.resolveOneToOne(dao, ordersDetails, OrderDetail.class)
+                .foreach((objectOrderDetail) -> {
+                    assert (objectOrderDetail.getProduct() != null);
+                });
+    }
+
+    /*@Test
     public void getAll() throws Exception {
         Dataset<Row> products = dao.getAllAsDataset(Product.class);
 
         SparkSQLRelationResolver.resolveOneToOne(dao, products, Product.class)
                 .foreach((objectProduct) -> {
-                    assert(objectProduct.getCategory() != null);
+                    assert (objectProduct.getCategory() != null);
                 });
 
 
@@ -81,7 +164,7 @@ public class SparkSQLDAOTest {
         StructType categorySchema = EntityReflection.reflectEntityToSparkSchema(Category.class);
         Dataset<Row> productsDataset = dao.getAndJoinAsDataset(Product.class);
 
-        for(StructField field : productsSchema.fields()) {
+        for (StructField field : productsSchema.fields()) {
             try {
                 productsDataset.schema().fieldIndex(field.name());
             } catch (IllegalArgumentException e) {
@@ -89,7 +172,7 @@ public class SparkSQLDAOTest {
             }
         }
 
-        for(StructField field : categorySchema.fields()) {
+        for (StructField field : categorySchema.fields()) {
             try {
                 if (!field.name().equals("id"))
                     assert (productsDataset.schema().fieldIndex(field.name()) >= 0);
@@ -97,6 +180,6 @@ public class SparkSQLDAOTest {
                 assert (false) : "Field '" + field.name() + "' doesn't exist";
             }
         }
-    }
+    }*/
 
 }
